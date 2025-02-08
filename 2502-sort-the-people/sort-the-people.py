ce880e4ee7870ab7905b7f0f9448f922287faba6
Ermias-Tekilemarkos <122928255+Ermi-Tekk21@users.noreply.github.com>
2025-02-08 00:59:56 -0800

    Time: 932 ms (5.01%) | Memory: 18.2 MB (39.83%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,12 +1,20 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # Pair names and heights together
-        people = list(zip(names, heights))
-
-        # Sort by height in descending order
-        people.sort(key=lambda x: x[1], reverse=True)
-
-        # Extract the sorted names
-        sorted_names = [person[0] for person in people]
-
-        return(sorted_names)
+        namedHeights = list((key, value) for key, value in zip(names, heights))
+        size = len(heights)
+        for i in range (size-1):
+            for j in range (0, size-i-1):
+                if (heights[j] > heights [j+1]):
+                    heights [j], heights [j+1] = heights[j+1], heights[j]
+        listedName=[]
+        for i in heights:
+            print (i)
+            for j in range (len(namedHeights)): 
+                    if namedHeights[j][1] == i:
+                        listedName.append(namedHeights[j][0])
+        size = len(listedName)
+        res = []
+        for i in range (size):
+            res.append(listedName[-i-1])
+                        
+        return (res)
