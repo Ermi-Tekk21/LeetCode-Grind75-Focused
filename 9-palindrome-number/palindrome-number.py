d9bf4c6310334f8b1f3e131d9ebeca25be4b30ff
Ermias-Tekilemarkos <122928255+Ermi-Tekk21@users.noreply.github.com>
2025-02-10 14:52:48 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.94%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,36 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        n = str(x)
-        l = len(n)
-        res = True
-        counter = 0
-        odd_route = int((l-1)/2)
-        even_route = int((l)/2)
-        if l % 2 == 0:
-            for i in range(even_route):
-                initial_index = i
-                back_index = (-1*i)-1
-                front = n[initial_index]
-                back = n[back_index]
-                if front.isdigit() and back.isdigit():
-                    if int(front) == int(back):
-                        counter += 1
-            if counter == even_route:
-                res = True
-            else:
-                res = False
-        else:
-            for i in range(odd_route):
-                initial_index = i
-                back_index = (-1*i)-1
-                front = n[initial_index]
-                back = n[back_index]
-                if front.isdigit() and back.isdigit():
-                    if int(front) == int(back):
-                        counter += 1
-            if counter == odd_route:
-                res = True
-            else:
-                res = False
-
-        return res
+        nums = str(x)
+        size = len(nums)
+        rPtr = size - 1
+        
+        for i in range (size // 2):
+            if nums[i] != nums[rPtr - i]: 
+                return False
+        return True
