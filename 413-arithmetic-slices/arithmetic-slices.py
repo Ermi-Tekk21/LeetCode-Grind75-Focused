3b6621e5cc50824eb765dd244e4e4298f20641c2
Ermias-Tekilemarkos <122928255+Ermi-Tekk21@users.noreply.github.com>
2025-02-13 04:37:00 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.62%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.py
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -1,10 +1,25 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [0] * n
-        ans = 0
-        for i in range(2, n):
-            if nums[i-1] - nums[i-2] == nums[i] - nums[i-1]:
-                dp[i] = dp[i-1] + 1
-            ans += dp[i]
-        return ans
+        def freq_sliding_win(cont_array_size):
+            k=3
+            sum = 0
+            for i in range (1,cont_array_size-k+2):
+                sum += i
+            return (sum)
+        tot_freq = 0
+        len_cont_array = 2
+        
+        for i in range (1, len(nums)-1):
+            ub = nums[i+1] - nums[i]
+            lb = nums[i] - nums[i-1]
+            
+            
+            if ub == lb:
+                len_cont_array +=1
+                
+            else:
+                tot_freq += freq_sliding_win(len_cont_array)
+                len_cont_array = 2
+                
+        tot_freq += freq_sliding_win(len_cont_array)
+        return (tot_freq)
