8b79bf9502a8689f92a697f333266dd9bd1d515d
Ermias-Tekilemarkos <122928255+Ermi-Tekk21@users.noreply.github.com>
2025-02-24 00:05:26 +0300

    Time: 42 ms (29.35%) | Memory: 17.8 MB (48.77%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,7 +1,20 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res = ""
-        l = min(len(word1), len(word2))
-        for i in range(l):
-            res += word1[i] + word2[i]
-        return res + word1[l:] + word2[l:]
+        n = len(word1 + word2)
+        word1 = list(word1)
+        word2 = list(word2)
+        word1.reverse()
+        word2.reverse()
+        newStr = ""
+        
+        for i in range(n):
+            
+            if i % 2 == 0 and word1:
+                newStr += word1.pop()
+            elif word2: 
+                newStr += word2.pop()
+        word1.reverse()     
+        word2.reverse()     
+        newStr += ("").join(word1)
+        newStr += ("").join(word2)
+        return newStr
