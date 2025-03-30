05ab3143755c8e178407666dbbb675f6d7a6d445
Ermias-Tekilemarkos <122928255+Ermi-Tekk21@users.noreply.github.com>
2025-03-30 22:17:45 +0300

    Time: 51 ms (81.63%) | Memory: 28.6 MB (71.44%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,11 +1,24 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = nums[:k]
-        heapq.heapify(heap)
-        
-        for num in nums[k:]:
-            if num > heap[0]:
-                heapq.heappop(heap)
-                heapq.heappush(heap, num)
-        
-        return heap[0]
+        # Initialize an empty list
+        k_numbers_min_heap = []
+
+        # Add first k elements to the list
+        for i in range(k):
+            k_numbers_min_heap.append(nums[i])
+
+        # Convert the list into a min-heap
+        heapq.heapify(k_numbers_min_heap)
+
+        # Loop through the remaining elements in the 'nums' array
+        for i in range(k, len(nums)):
+            # Compare the current element with the minimum
+            # element (root) of the min-heap
+            if nums[i] > k_numbers_min_heap[0]:
+                # Remove the smallest element
+                heapq.heappop(k_numbers_min_heap)
+                # Add the current element
+                heapq.heappush(k_numbers_min_heap, nums[i])
+
+        # The root of the heap has the Kth largest element
+        return k_numbers_min_heap[0]
